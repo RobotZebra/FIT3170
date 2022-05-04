a7a4e691e691ac0d235f9082f91cde61cc2d4119
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-05-04 16:42:31 +1000

    added a simple purple form submission button

--- app/pages/MyPregnancyPage.js
+++ app/pages/MyPregnancyPage.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Text, View } from "react-native";
+import {PurpleButton, PurpleFormSubmissionButton} from "../components/Buttons";
 
 
 export function MyPregnancyPage() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>My pregnancy tab</Text>
+        <PurpleFormSubmissionButton
+            width={343}
+            height={51}
+            title={"Submit"}
+            onPress={() => console.log("form submit")}
+        />
+        <PurpleButton
+            width={321}
+            height={51}
+            title={"press me"}
+            onPress={() => console.log("press me")}
+        />
     </View>
   );
 }
--- app/styles/GlobalStyles.js
+++ app/styles/GlobalStyles.js
@@ -1,2 +1,14 @@
 const GlobalStyles = {
-}
+    purpleFormSubmissionButtonContainer: {
+        backgroundColor: "#4E1965",
+        borderRadius: 100,
+        paddingTop: 16,
+        paddingBottom: 16,
+        paddingLeft: 32,
+        paddingRight: 32,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+};
+
+export default GlobalStyles;
