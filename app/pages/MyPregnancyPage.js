fe1dcb9bd85cb339f4c217c609e57f25b34f465d
Jeremy <jpongfam@gmail.com>
2022-05-05 21:48:56 +1000

    added carousel to mypregnancy page

--- app/pages/MyPregnancyPage.js
+++ app/pages/MyPregnancyPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { StageCarousel } from "../components/my-pregnancy/StageCarousel";
 
 export function MyPregnancyPage() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>My pregnancy tab</Text>
+      <Text style>My pregnancy tab</Text>
+      <StageCarousel />
     </View>
   );
 }
