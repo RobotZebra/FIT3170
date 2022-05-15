d66b0b27014ae4a57c192892e05c93921c83ebc1
Julius Jones <jjon0014@student.monash.edu>
2022-05-15 16:36:53 +1000

    circular progress done, can be open to further additions

--- app/components/BabySize.js
+++ app/components/BabySize.js
@@ -3,26 +3,33 @@ import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import CircularProgress from 'react-native-circular-progress-indicator';
 
-export default function BabySize() {
+export function BabySize() {
        const [value, setValue] = useState(0);
         // Not sure how we are handling this yet:
         const pregnancyDayValue = 60;
+        const monthValue = Math.ceil(pregnancyDayValue / 30)
         const averagePregnancyDuration = 280;
+        const pregnancyRemainingTime = averagePregnancyDuration - pregnancyDayValue;
         const pregnancyProportion = (pregnancyDayValue / averagePregnancyDuration) * 100;
     return (
         <View style = {styles.container}>
             <CircularProgress
-                radius = {90}
+                radius = {120}
                 value = {pregnancyProportion}                    // the value we need
                 textColor = '#222'
-                fontsize = {20}
-                valueSuffix = {'%'}
-                inActiveStrokeColor = {'#2ecc71'}
-                inActiveStrokeOpacity = {0.2}
+                title = {pregnancyRemainingTime + ' days left'}
+                subtitle = {"Month " + monthValue}
+                showProgressValue = {false}
+                activeStrokeColor = {'purple'}
+                inActiveStrokeColor = {'lightgray'}
+                inActiveStrokeOpacity = {1}
                 inActiveStrokeWidth = {6}
-                duration = {3000}
-                onAnimationComplete = {() => setValue(50)}
+                duration = {0}
+                onAnimationComplete={() => setValue(50)}
             />
+
+
+            <StatusBar style="auto"/>
         </View>
     );
 
