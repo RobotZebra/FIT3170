93ccbb70fb955a970d4feec4cfe632428142537b
Shenyi Hu <shuu0020@student.monash.edu>
2022-05-17 00:06:10 +1000

    Added appointment component in Home page.

--- app/pages/MyPregnancyPage.js
+++ app/pages/MyPregnancyPage.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Text, View } from "react-native";
+import {RecommendedAppointment} from "../components/RecommendedAppointment";
 
 export function MyPregnancyPage() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>My pregnancy tab</Text>
-    </View>
-  );
+    return <>
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>My pregnancy tab</Text>
+        </View>
+
+        <View style={{ height: 200 }}>
+            <RecommendedAppointment />
+        </View>
+
+
+    </>;
 }
