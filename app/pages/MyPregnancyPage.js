a315540ed378499fb4e73bdae432e7b55b494a35
jer <jpongfam@gmail.com>
2022-05-17 17:52:01 +1000

    Merge branch 'sizeOfBaby' into Team1

--- app/pages/MyPregnancyPage.js
+++ app/pages/MyPregnancyPage.js
@@ -2,16 +2,23 @@ import React from "react";
 import { Text, View } from "react-native";
 import { StageCarousel } from "../components/my-pregnancy/StageCarousel";
 import { VisualisationOfBaby } from "../components/VisualisationOfBaby";
+import { BabySize } from "../components/BabySize";
 
 export function MyPregnancyPage() {
-  return (
+  const babyViewText = BabySize();
+  return <>
+    <View>
+      {babyViewText}
+    </View>
+
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>My pregnancy tab</Text>
       <View style={{ height: 400 }}>
-            <VisualisationOfBaby />
-        </View>
+        <VisualisationOfBaby />
+      </View>
       <View style={{ height: 355 }}>
         <StageCarousel />
       </View>
     </View>
-  );
+  </>
+}
