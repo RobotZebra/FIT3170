0785a726660288974c6e1a13920804293f019798
ficrowe <fcro0003@student.monash.edu>
2022-05-19 13:24:30 +1000

    inital refactor to change design to new collapsible accordion style

--- app/pages/PractitionersPage.js
+++ app/pages/PractitionersPage.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import React, {useState, useEffect,Component} from "react";
+import { Text, View, StyleSheet, SafeAreaView, ScrollView, TouchableOpacity,
+  LayoutAnimation, UIManager, Platform, Button, Linking } from "react-native";
 import ContactCard from "../components/ContactCard";
 
 export function PractitionersPage() {
@@ -14,35 +15,186 @@ export function PractitionersPage() {
     subtitle: "Clayton",
     favourite: false
   };
+
   const MATERNITY_KEY_CONTACTS = "Maternity Services Key Contacts"
+  const MATERNITY_KEY_CONTACTS_TEXT = "Ski-bi dibby dib yo da dub dub\
+  Yo da dub dub\
+  Ski-bi dibby dib yo da dub dub\
+  Yo da dub dub\
+  (I'm the Scatman)\
+  Ski-bi dibby dib yo da dub dub\
+  Yo da dub dub\
+  Ski-bi dibby dib yo da dub dub\
+  Yo da dub dub"
   const SPECIALIST_OBSTETRICIAN_CONTACTS = "Specialist Obstetrician Contacts"
+  const SPECIALIST_OBSTETRICIAN_CONTACTS_TEXT = "Ba-da-ba-da-ba-be bop bop bodda bope\
+  Bop ba bodda bope\
+  Be bop ba bodda bope\
+  Bop ba bodda\
+  Ba-da-ba-da-ba-be bop ba bodda bope\
+  Bop ba bodda bope\
+  Be bop ba bodda bope\
+  Bop ba bodda bope"
+
+  const CONTENT = [
+    {
+        isExpanded: false,
+        category_name: MATERNITY_KEY_CONTACTS,
+        subcategory: [
+            {id: 1, val: 'Description', descriptionVal: MATERNITY_KEY_CONTACTS_TEXT},
+            {id: 2, val: 'Call'},
+            {id: 3, val: 'Email', email: "example@gmail.com"},
+            {id: 4, val: 'Fax'},
+        ]
+
+
+    },
+    {
+        isExpanded: false,
+        category_name: SPECIALIST_OBSTETRICIAN_CONTACTS,
+        subcategory: [
+          {id: 1, val: 'Description', descriptionVal: SPECIALIST_OBSTETRICIAN_CONTACTS_TEXT},
+          {id: 2, val: 'Call'},
+          {id: 3, val: 'Email', email: "example@gmail.com"},
+          {id: 4, val: 'Fax'},
+        ]
+    }
+  ];
+
+  const openGmail = () => {
+    Linking.openURL('mailto:support@example.com')
+     };
+
+ const openPhone = (phoneNumber) => {
+     Linking.openURL(`tel:${phoneNumber}`)
+ }
+
+ const PractitionerComponents = ({props}) => {
+     if (props.val == "Description"){
+        var description = props.descriptionVal;
+        return(
+            <Text style={styles.text}>{description}</Text>
+        )
+
+     }
+     else if (props.val == "Email") {
+         let valuesMaps={
+            email: props.email
+          }
+
+         return (
+             <Button  style={{fontSize: 20, color: 'green'}}
+                styleDisabled={{color: 'red'}}
+
+                onPress={() => showInMapClicked(valuesMaps)}
+                 title="Email">
+             </Button> )
+     }
+     else if (props.val == "Call"){
+         return (
+                     <Button  style={{fontSize: 20, color: 'green'}}
+                        styleDisabled={{color: 'red'}}
+                        onPress={() => openPhone(props)}
+                         title="Call">
+                     </Button> )
+     }
+     else 
+     if (props.val == "Fax") {
+      return (
+        <Button  style={{fontSize: 20, color: 'green'}}
+           styleDisabled={{color: 'red'}}
+          //  onPress={() => openPhone(props)}
+            title="Fax">
+        </Button> )
+     }
+     else {
+         return null
+
+     };
+ };
+
+  const ExpandableComponent = ({item, onClickFunction}) => {
+    const [layoutHeight, setLayoutHeight] = useState(0);
+
+    useEffect(() => {
+        if(item.isExpanded) {
+            setLayoutHeight(null);
+        } else {
+            setLayoutHeight(0);
+        }
+    }, [item.isExpanded])
+
+    return (
+      <View>
+          <TouchableOpacity
+              style={styles.item}
+              onPress={onClickFunction}
+          >
+              <Text style={styles.itemText}>
+                  {item.category_name}
+              </Text>
+          </TouchableOpacity>
+          <View
+              style={{
+              height: layoutHeight,
+              overflow: 'hidden'
+              }}
+          >
+              {
+                  item.subcategory.map((object, key) => (
+                      <TouchableOpacity
+                          key = {key}
+                          style = {styles.content}
+                          >
+
+                          <PractitionerComponents props =  {object}/>
+                          <View style={styles.separator }/>
+
+                      </TouchableOpacity>
+                  ))
+              }
+          </View>
+      </View>
+    )
+}
+const [listDataSource, setListDataSource] = useState(CONTENT);
+const updateLayout = (index) => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    const array = [...listDataSource];
+    array.map((value, placeIndex) =>
+            placeIndex === index
+            ? (array[placeIndex]['isExpanded']) = !array[placeIndex]['isExpanded']
+            : (array[placeIndex]['isExpanded']) = false
+      );
+    
+    setListDataSource(array)
+}
+if (Platform.OS === 'android'){
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.heading1}>
-        {MATERNITY_KEY_CONTACTS}
-      </Text>
-      <View style={{ paddingTop: 20, alignItems: "center"}}>
-        <ContactCard props={maternityContactCardProps}>
-        </ContactCard>
-      </View>
-      <Text>
-      {"\n"}
-      {"\n"}
-      </Text>
-      <Text style={styles.heading1}>
-        {SPECIALIST_OBSTETRICIAN_CONTACTS}
-      </Text>
-      <View style={{ paddingTop: 20, alignItems: "center"}}>
-        <ContactCard props={obstetricianContactCardProps}>
-        </ContactCard>
-      </View>
+      <ScrollView>
+                    {
+                        listDataSource.map((item, key) =>(
+                            <ExpandableComponent
+                                key={item.category_name}
+                                item={item}
+                                onClickFunction={() => {
+                                    updateLayout(key)
+                                }}
+                            />
+                        ))
+                    }
+      </ScrollView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: "center",
     flex: 1
   },
   heading1: {
@@ -52,4 +204,46 @@ const styles = StyleSheet.create({
     fontSize: 25,
     padding: 15
   },
+  header: {
+      flexDirection: 'row',
+      padding: 10
+  },
+  titleText: {
+      flex: 1,
+      fontSize: 20,
+      fontWeight: 'bold',
+  },
+  headerButton: {
+      textAlign: 'center',
+      justifyContent: 'center',
+      fontSize: 18
+  },
+  item: {
+      backgroundColor: 'purple',
+      padding: 20
+  },
+  itemText: {
+      fontSize: 16,
+      fontWeight: '500',
+      color:'white'
+  },
+  content: {
+      paddingLeft: 10,
+      paddingRight: 10,
+      backgroundColor: '#fff'
+  },
+  text: {
+      fontSize: 13,
+      padding: 20,
+      color: 'black',
+      alignItems: "flex-start",
+      justifyContent: "space-between",
+      textAlignVertical: "center"
+  },
+  separator:{
+      height: 0.5,
+      backgroundColor: '#a089ad',
+      width: '100%'
+
+  }
 });
