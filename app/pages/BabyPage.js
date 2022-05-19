466402eddb3f8e2253131f5a034e9f1899df27a9
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-05-19 15:39:53 +1000

    comments and guidelines for using dropdown added

--- app/pages/BabyPage.js
+++ app/pages/BabyPage.js
@@ -1,15 +1,22 @@
-import { ScrollView, Text } from "react-native";
+import { ScrollView, View, Text } from "react-native";
 import { DropDown } from "../components/DropDown";
 import { AntDesign } from "@expo/vector-icons";
 
 export default function BabyPage() {
   return (
     <ScrollView style={{ width: "100%" }}>
+      <View
+        style={{
+          // just some empty space for the scroll view here
+          height: 40,
+        }}
+      ></View>
       <DropDown
         header={"Growth"}
         subHeader={"What are the stages of my baby's development?"}
-        information={<Text>Some info</Text>}
+        information={<Text>put information component here!!!</Text>}
         icon={
+          // change the icons here
           <AntDesign
             name="picture"
             size={24}
@@ -21,7 +28,7 @@ export default function BabyPage() {
       <DropDown
         header={"Topic Title"}
         subHeader={"topic subtitle"}
-        information={<Text>Some info</Text>}
+        information={<Text>place holder!!!</Text>}
         icon={
           <AntDesign
             name="picture"
