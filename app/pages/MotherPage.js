626e67533db2aae20998ea8e368b14ea26801f85
Robert <rgud0002@student.monash.edu>
2022-05-20 20:37:53 +1000

    adding symptoms, nutrition and mood to Mother page

--- app/pages/MotherPage.js
+++ app/pages/MotherPage.js
@@ -1,5 +1,56 @@
 import { View } from "react-native";
+import { Text, View } from "react-native";
+import { DropDown } from "../components/DropDown";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function MotherPage() {
-  return <View></View>;
+  return 
+  <View
+  style={{ flex: 1, justifyContent: "flex-start", alignItems: "center" }}
+  >
+  <DropDown
+  //symptoms
+        header={"Signs and Symptoms"}
+        subHeader={"What will I experience during pregnancy?"}
+        information={<Text>Some info</Text>}
+        icon={
+          <MaterialCommunityIcons
+            name="heart"
+            size={30}
+            color="white"
+            style={{ margin: 5 }}
+          />
+        }
+        />
+  <DropDown
+  //nutrition
+        header={"Nutrition"}
+        subHeader={"What should and shouldn't I eat during pregnancy?"}
+        information={<Text>Some info</Text>}
+        icon={
+          <FontAwesome5
+            name="silverware-fork-knife"
+            size={30}
+            color="white"
+            style={{ margin: 5 }}
+          />
+        }
+        />
+  <DropDown
+  //mood
+        header={"Mood"}
+        // maybe it should be behavioural instead of psychological?
+        subHeader={"What psychological changes might I expererience?"}
+        information={<Text>Some info</Text>}
+        icon={
+          <FontAwesome5
+            name="baby"
+            size={30}
+            color="white"
+            style={{ margin: 10 }}
+          />
+        }
+      />
+    </View>
 }
