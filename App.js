b459aff7267753c1d33e8edcf66b31fc620cdabf
Jason Marshall <jasonmarshall19@gmail.com>
2022-05-23 22:32:43 +1000

    Refactoring App.js

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StyleSheet } from "react-native";
 import BottomTabNavigator from "./app/navigation/FooterTabNavigator";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { useFonts, Roboto_100Thin, Roboto_100Thin_Italic, Roboto_300Light, Roboto_300Light_Italic, Roboto_400Regular, Roboto_400Regular_Italic, Roboto_500Medium, Roboto_500Medium_Italic, Roboto_700Bold, Roboto_700Bold_Italic, Roboto_900Black, Roboto_900Black_Italic} from '@expo-google-fonts/roboto'
+import {
+  useFonts,
+  Roboto_100Thin,
+  Roboto_100Thin_Italic,
+  Roboto_300Light,
+  Roboto_300Light_Italic,
+  Roboto_400Regular,
+  Roboto_400Regular_Italic,
+  Roboto_500Medium,
+  Roboto_500Medium_Italic,
+  Roboto_700Bold,
+  Roboto_700Bold_Italic,
+  Roboto_900Black,
+  Roboto_900Black_Italic,
+} from "@expo-google-fonts/roboto";
 import AppLoading from "expo-app-loading";
 
 export default function App() {
-
   const [fontsLoaded] = useFonts({
     Roboto_100Thin,
     Roboto_100Thin_Italic,
@@ -18,11 +31,11 @@ export default function App() {
     Roboto_700Bold,
     Roboto_700Bold_Italic,
     Roboto_900Black,
-    Roboto_900Black_Italic
-  })
+    Roboto_900Black_Italic,
+  });
 
   if (!fontsLoaded) {
-    return <AppLoading />
+    return <AppLoading />;
   }
 
   return (
