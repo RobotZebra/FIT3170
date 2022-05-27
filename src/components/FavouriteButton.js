b3966157cec7176ba0a12fcfcbe3b7f972ca3c50
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-05-28 08:56:45 +1000

    replaced class component with function component

--- src/components/FavouriteButton.js
+++ src/components/FavouriteButton.js
@@ -1,52 +1,48 @@
-import React, {Component} from "react";
-import {TouchableOpacity, View, StyleSheet} from "react-native";
+import React from "react";
+import { TouchableOpacity, View, StyleSheet } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const MATERNITY_KEY_CONTACTS = "Maternity Services Key Contacts"
-const MATERNITY_KEY_CONTACTS_TEXT = "Please fax initial referrals. For all subsequent queries about hospital\
+const MATERNITY_KEY_CONTACTS = "Maternity Services Key Contacts";
+const MATERNITY_KEY_CONTACTS_TEXT =
+  "Please fax initial referrals. For all subsequent queries about hospital\
     site allocation, please contact our maternity shared care coordinator\
-    (for shared care patients) or midwife manager of clinic."
-const SPECIALIST_OBSTETRICIAN_CONTACTS = "Specialist Obstetrician Contacts"
-const SPECIALIST_OBSTETRICIAN_CONTACTS_TEXT = "Please fax initial referrals. For all subsequent queries about hospital\
-    site allocation, please contact our maternity shared care coordinator\    (for shared care patients) or midwife manager of clinic."
-    
+    (for shared care patients) or midwife manager of clinic.";
+const SPECIALIST_OBSTETRICIAN_CONTACTS = "Specialist Obstetrician Contacts";
+const SPECIALIST_OBSTETRICIAN_CONTACTS_TEXT =
+  "Please fax initial referrals. For all subsequent queries about hospital\
+    site allocation, please contact our maternity shared care coordinator    (for shared care patients) or midwife manager of clinic.";
 
-export default class FavouriteButton extends Component {
+export default function FavouriteButton() {
+  const [favourited, setFavourited] = useState(false);
 
-
-    state = {
-        favourited: false
-    }
-
-    constructor(isFavourited) {
-        this.state.favourited = isFavourited
-        console.log('CONSTRUCTOR')
-        
-    }
-
-    onPress = () => {
-        console.log(this.state.favourited)
-        this.state.favourited = !this.state.favourited
-        this.render()
-    }
-
-    render() {
-        console.log(this.state.favourited)
-        return (
-            <TouchableOpacity activeOpacity={0.5} onPress={() => {this.onPress}}>
-                <View style={[{
-                            flexDirection: "row"
-                        }]}>
-                    {this.state.favourited ? <MaterialCommunityIcons name="heart" color="white" size={39} style={styles.headerIcon}/> : <MaterialCommunityIcons name="heart-outline" color="white" size={39} style={styles.headerIcon}/>}
-                </View>
-            </TouchableOpacity>
-        ) 
-    }    
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={() => setFavourited(!favourited)}
+    >
+      <View style={{ flexDirection: "row" }}>
+        {favourited ? (
+          <MaterialCommunityIcons
+            name="heart"
+            color="white"
+            size={39}
+            style={styles.headerIcon}
+          />
+        ) : (
+          <MaterialCommunityIcons
+            name="heart-outline"
+            color="white"
+            size={39}
+            style={styles.headerIcon}
+          />
+        )}
+      </View>
+    </TouchableOpacity>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    headerIcon: {
-        paddingLeft: 90
-    }
+  headerIcon: {
+    paddingLeft: 90,
+  },
 });
