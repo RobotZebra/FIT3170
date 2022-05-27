ad8de222bfc9a209ec0137baae00963723972e5b
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-05-28 08:59:34 +1000

    removed useless imporst from wiki page

--- src/pages/WikiPage.js
+++ src/pages/WikiPage.js
@@ -1,13 +1,6 @@
-import { ScrollView, Animated, View, Text } from "react-native";
+import { ScrollView } from "react-native";
 import { DropDown } from "../components/DropDown";
 
-function PlaceholderInformation() {
-  return (
-    <Animated.View>
-      <Text>Put some information here!</Text>
-    </Animated.View>
-  );
-}
 
 export default function WikiPage(props) {
   const { dropDownList } = props;
