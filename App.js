8fd01caf4d41ff0cb74da801749399d1ab913527
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-05-28 14:06:37 +1000

    fonts extracted into app wrapper with useFonts hook

--- App.js
+++ App.js
@@ -1,72 +1,31 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomTabNavigator from './src/components/navigation/FooterTabNavigator';
+import BottomTabNavigator from '~/components/navigation/FooterTabNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import {
-  useFonts,
-  Roboto_100Thin,
-  Roboto_100Thin_Italic,
-  Roboto_300Light,
-  Roboto_300Light_Italic,
-  Roboto_400Regular,
-  Roboto_400Regular_Italic,
-  Roboto_500Medium,
-  Roboto_500Medium_Italic,
-  Roboto_700Bold,
-  Roboto_700Bold_Italic,
-  Roboto_900Black,
-  Roboto_900Black_Italic,
-} from '@expo-google-fonts/roboto';
-import AppLoading from 'expo-app-loading';
-import LoginScreen from './src/pages/LoginScreen';
-import RegisterScreen from './src/pages/RegisterScreen';
-import ResetPasswordScreen from './src/pages/ResetPasswordScreen';
+import LoginScreen from '~/pages/LoginScreen';
+import RegisterScreen from '~/pages/RegisterScreen';
+import ResetPasswordScreen from '~/pages/ResetPasswordScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import Roboto from '~/components/fonts/Roboto';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    Roboto_100Thin,
-    Roboto_100Thin_Italic,
-    Roboto_300Light,
-    Roboto_300Light_Italic,
-    Roboto_400Regular,
-    Roboto_400Regular_Italic,
-    Roboto_500Medium,
-    Roboto_500Medium_Italic,
-    Roboto_700Bold,
-    Roboto_700Bold_Italic,
-    Roboto_900Black,
-    Roboto_900Black_Italic,
-  });
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
     <SafeAreaProvider>
       <RootSiblingParent>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={RegisterScreen} />
-            <Stack.Screen name="Reset" component={ResetPasswordScreen} />
-            <Stack.Screen name="Dashboard" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <Roboto>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Register" component={RegisterScreen} />
+              <Stack.Screen name="Reset" component={ResetPasswordScreen} />
+              <Stack.Screen name="Dashboard" component={BottomTabNavigator} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </Roboto>
       </RootSiblingParent>
     </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
