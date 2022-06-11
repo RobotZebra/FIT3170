dc74e75560b08e907edf0f8e4ad334b887cb0c34
dylanclements <58539346+dylanclements@users.noreply.github.com>
2022-06-11 10:01:08 +1000

    Added async storage for rtk query exrample

--- src/api/firestoreApi.js
+++ src/api/firestoreApi.js
@@ -1,5 +1,6 @@
 import { getDocs, getFirestore, query, collection } from '@firebase/firestore';
 import { createApi, fakeBaseQuery } from '@reduxjs/toolkit/query/react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebaseApp from '~/services/firebase/config';
 
 /* 
@@ -12,7 +13,7 @@ export const firestoreApi = createApi({
   // baseQuery isn't used here, but it's normally used to define
   // base URLs and shared request headers
   baseQuery: fakeBaseQuery(),
-  // define our endpoints here, each endpoint results in an auto generated hook
+  // function that returns an object that defines our endpoints. each endpoint results in an auto generated hook
   endpoints: (builder) => ({
     // this endpoint will pull all practitioners from the DB
     getPractitioners: builder.query({
@@ -23,18 +24,28 @@ export const firestoreApi = createApi({
         try {
           const querySnapshot = await getDocs(q);
           console.log(querySnapshot);
+          // you must return a { data: object } if query is successful
           return {
             data: querySnapshot,
           };
         } catch (err) {
+          // you must return { error: object } if query is not successful
           return { error: err };
         }
       },
       // you can edit the response before returning it with transformResponse
       transformResponse: (response) => response,
       // using this function you can perform tasks before, during or after the query
-      // for example, save some stuff to AsyncStorage before returning response
-      onQueryStarted: async () => {},
+      onQueryStarted: async (args, { queryFulfilled }) => {
+        // do things here before the query is complete
+        console.log(args);
+
+        // waiting for the query to be complete
+        const { data } = await queryFulfilled;
+
+        // do things here after the query is complete, like storing data in local storage
+        await AsyncStorage.setItem('practitioner-collection', JSON.stringify(data));
+      },
     }),
   }),
 });
